8cfd8ab57f2666501061ab3c044e8c3a027117c1
Phelipy Julião <124466082+phelipywithph@users.noreply.github.com>
2025-11-11 01:50:52 +0000

    segundo commit

--- empresa/dao/base_dao.py
+++ empresa/dao/base_dao.py
@@ -21,12 +21,12 @@ class BaseDAO(ABC, Generic[T]):
   # Do formato JSON (dict) para modelo de dados (T)
   @abstractmethod
   def to_model(self, data: dict) -> T:
-    pass
+    pass #os filhos devem decidir como será feito a implementação
 
   # Do modelo de dados (T) para formato JSON (dict)
   @abstractmethod
   def to_dict(self, model: T) -> dict:
-    pass
+    pass #os filhos devem decidir como será feito a implementação
 
   ### Create - função para criar algo na tabela:
   def create(self, model: T) -> Optional[T]: #recebe um modelo genérico T
@@ -43,7 +43,16 @@ class BaseDAO(ABC, Generic[T]):
             return None #retorna nada
     
   ### Read
-
+  def read_id(self, record_id: int) -> Optional[T]:
+    try:
+      response = self._client.table(self._table_name).select('*').execute()
+      if response.data:
+        return [self.to_model(item) for item in response.data]
+      return [] #retorna uma lista vazia
+    except Exception as e:
+      print(f'Erro ao buscar todos os registros: {e}')
+      return [] #retorna uma lista vazia
+  
   # Retorna todos os valores de uma tabela
   def read_all(self) -> List[T]:
     try:
@@ -56,5 +65,26 @@ class BaseDAO(ABC, Generic[T]):
       return []
     
   ### Update
+  def update(self, record_id: int, model: T) -> Optional[T]:
+        #
+        try:
+            data = self.to_dict(model)
+            response = self._client.table(self._table_name).update(data).eq('id', record_id).execute()
+            if response.data:
+                return self.to_model(response.data[0])
+            return None #retorna nada
+        #caso de erro
+        except Exception as e:
+            print(f"Erro ao atualizar registro: {e}") #mensagem de erro
+            return None #retorna nada
   
   ### Delete
+  def delete(self, record_id: int) -> bool: #deve retornar valores lógicos (True ou False)
+        #tratamento de erros
+        try:
+            response = self._client.table(self._table_name).delete().eq('id', record_id).execute()
+            return bool(response.data) #retorna verdadeiro se o registro for deletado
+        #caso de erro
+        except Exception as e:
+            print(f"Erro ao deletar registro: {e}") #mensagem de erro
+            return False #retorna falso
